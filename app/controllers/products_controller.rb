5a49c94305df92c4fae24816f7ed7b20421eaedb
Ren Ant <eurekavixen@gmail.com>
2024-10-02 08:58:14 +0800

    added crud functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,26 +5,39 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
 
+    if @product.save
+      flash[:notice] = 'Product created successfully!'
+      redirect_to products_path
+    else
+      flash.now[:alert] = 'Product creation failed'
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @product.update(product_params)
+      flash[:notice] = 'Product updated successfully!'
+      redirect_to products_path
+    else
+      flash.now[:alert] = 'Product update failed'
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @product.destroy
+    flash[:notice] = 'Product deleted successfully!'
     redirect_to products_path
   end
 
