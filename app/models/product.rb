883582174b3bc03beb66466554e0cc6b91566fa6
Renier Antonio <eurekavixen@gmail.com>
2024-10-02 09:14:02 +0800

    added presence validations for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
+  NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at"]
+  VALIDATABLE_ATTRS = Product.attribute_names.reject { |attr| NON_VALIDATABLE_ATTRS.include?(attr) }
 
+  validates_presence_of VALIDATABLE_ATTRS
 end
