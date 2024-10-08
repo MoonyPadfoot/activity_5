f31e65994bb7574734214997fd6ff8776f1f91b8
Renier Antonio <eurekavixen@gmail.com>
2024-10-08 09:01:19 +0800

    modified: authentication from email to username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_07_054135) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_08_003714) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -91,8 +91,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_07_054135) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "warranties", charset: "utf8mb4", force: :cascade do |t|
