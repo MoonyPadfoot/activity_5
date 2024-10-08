fd78075d2cb88bced608db8b04e29d27e3152462
Renier Antonio <eurekavixen@gmail.com>
2024-10-08 16:13:08 +0800

    added: my shop action for customers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,16 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_search_filter, only: [:index, :my_shop]
 
   def index
-    @products = Product.all
-                       .search_by_name(params[:name])
-                       .filter_by_available(params[:available] || true)
-                       .filter_by_quantity(params[:quantity_min], params[:quantity_max])
-                       .filter_by_price(params[:price_min], params[:price_max])
-                       .filter_by_released_at(params[:released_at_start], params[:released_at_end])
-                       .page(params[:page]).per(5)
+
   end
 
   def show; end
+
+  def my_shop
+
+  end
 
   def new
     @product = Product.new
@@ -56,5 +55,15 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :content, :image, :quantity, :price, :available, :released_at, :discount)
+  end
+
+  def set_search_filter
+    @products = Product.all
+                       .search_by_name(params[:name])
+                       .filter_by_available(params[:available] || true)
+                       .filter_by_quantity(params[:quantity_min], params[:quantity_max])
+                       .filter_by_price(params[:price_min], params[:price_max])
+                       .filter_by_released_at(params[:released_at_start], params[:released_at_end])
+                       .page(params[:page]).per(5)
   end
 end
