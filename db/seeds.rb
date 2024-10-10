1b9668c2069bd469083c761a45643a5c1d61cbdf
Renier Antonio <eurekavixen@gmail.com>
2024-10-10 12:49:56 +0800

    added: initial seed for product, review, and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  user = User.create!(email: Faker::Internet.email, username: Faker::Internet.username(specifier: 10), password: 'qwer4321', password_confirmation: "qwer4321")
+  puts "create user id: #{user.id}, email: #{user.email}. username: #{user.username}"
+end
+
+30.times do |i|
+  puts "start create #{i} product"
+  post = Product.create(name: Faker::Appliance.brand, content: Faker::Lorem.paragraph, quantity: Faker::Number.between(from: 50, to: 500),
+                        price: Faker::Number.decimal(l_digits: 5, r_digits: 2), available: Faker::Boolean.boolean(true_ratio: 0.4),
+                        discount: Faker::Number.decimal(l_digits: 2, r_digits: 1), image: Faker::LoremFlicker.image(size: "50x60", search_terms: ['appliance']),
+                        released_at: Faker::Date.between(from: '2000-02-02', to: '2024-09-25'))
+  (1..20).to_a.sample.times do
+    Review.create(content: Faker::Lorem.sentence, user: User.all.sample, product: product)
+  end
+  puts "finish #{i} product"
+end
